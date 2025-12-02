2167efa00a147bcffbc2076f507b3d8d532efc62
Apuuka2 <mainsteamakk27@gmail.com>
2025-12-02 16:49:49 +0500

    Добавьте файлы проекта.

---
+++ ProductPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bikbulatov41
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+
+            var currentProduct = Bikbulatov41Entities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProduct;
+
+            ComboType.SelectedIndex = 0;
+            UpdateProduct();
+        }
+        private void UpdateProduct()
+        {
+            var allProducts = Bikbulatov41Entities.GetContext().Product.ToList();
+            var currentProduct = allProducts.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) < 9.99)).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) < 14.99)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductDiscountAmount) < 100)).ToList();
+            }
+            currentProduct = currentProduct
+                .Where(p => p.ProductName.ToLower()
+                .Contains(TBoxSearch.Text.ToLower()))
+                .ToList();
+
+            if (RButtonDown.IsChecked == true)
+                currentProduct = currentProduct.OrderByDescending(p => p.ProductCost).ToList();
+
+            if (RButtonUp.IsChecked == true)
+                currentProduct = currentProduct.OrderBy(p => p.ProductCost).ToList();
+
+            ProductListView.ItemsSource = currentProduct;
+
+            TextCount.Text = $"{currentProduct.Count} из {allProducts.Count}";
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+    }
+}
